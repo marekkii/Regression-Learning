0f8e7bcf0300166d258cedc3c97b3567a6b87fea
Mark <markbramnauta@gmail.com>
2023-05-22 23:02:05 +0200

    Start Regression Classes

---
+++ Linear_Regression.py
@@ -0,0 +1,15 @@
+""" File with different Regression Classes """
+
+class LinearRegression():
+    def __init__(self):
+        pass
+
+
+class RigdeRegression():
+    def __init__(self):
+        pass
+
+
+class LassoRegression():
+    def __init__(self):
+        pass
