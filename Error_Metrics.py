a77803edfcd6636cd451d51c30b5896b64410596
Mark <markbramnauta@gmail.com>
2023-05-22 23:14:55 +0200

    Error Metrics Regression

---
+++ Error_Metrics.py
@@ -0,0 +1,9 @@
+""" File for Error Metrics """
+import numpy as np
+
+def mean_absolute_error() -> float:
+    pass
+
+def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    pass
+
